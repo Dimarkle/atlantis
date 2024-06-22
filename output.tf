953219a51e4a3e86d07f8a050139daa311d2189c
Dimarkle <118626944+Dimarkle@users.noreply.github.com>
2024-06-22 23:52:04 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -13,5 +13,23 @@ output "internal-ip-address_worker-1" {
 value = yandex_compute_instance.vms["worker-1"].network_interface[0].ip_address
 }
 
+output "master_ip_address_nat-master" {
+value = yandex_compute_instance.vms["worker-1"].fqdn
+}
 
+output "internal-ip-address_worker-2" {
+value = yandex_compute_instance.vms["worker-2"].network_interface[0].ip_address
+}
+
+output "master_ip_address_nat-master" {
+value = yandex_compute_instance.vms["worker-2"].fqdn
+}
+
+output "internal-ip-address_worker-3" {
+value = yandex_compute_instance.vms["worker-3"].network_interface[0].ip_address
+}
+
+output "master_ip_address_nat-master" {
+value = yandex_compute_instance.vms["worker-3"].fqdn
+}
 
