27dc7ae2a808e553a259dfe0781160a33514d6d4
Dimarkle <118626944+Dimarkle@users.noreply.github.com>
2024-06-22 23:16:30 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,32 +1,9 @@
-output "internal-ip-address-master" {
-value = "${yandex_compute_instance.master.network_interface.0.ip_address}"
-}
-output "master_ip_address_nat-master" {
-value = "${yandex_compute_instance.master.network_interface.0.nat_ip_address}"
-}
 
-
-output "internal-ip-address-worker-1" {
-value = "${yandex_compute_instance.worker-1.network_interface.0.ip_address}"
-}
-output "master_ip_address_nat-worker-1" {
-value = "${yandex_compute_instance.worker-1.network_interface.0.nat_ip_address}"
+## Outputs
+output "internal_ip_address_vm_master" {
+  value = yandex_compute_instance.vms["master"].network_interface[0].ip_address
 }
 
-
-output "internal-ip-address-worker-2" {
-value = "${yandex_compute_instance.worker-2.network_interface.0.ip_address}"
+output "fqdn_vm_master" {
+  value = yandex_compute_instance.vms["master"].fqdn
 }
-output "master_ip_address_nat-worker-2" {
-value = "${yandex_compute_instance.worker-2.network_interface.0.nat_ip_address}"
-}
-
-
-output "internal-ip-address-worker-3" {
-value = "${yandex_compute_instance.worker-3.network_interface.0.ip_address}"
-}
-output "master_ip_address_nat-worker-3" {
-value = "${yandex_compute_instance.worker-3.network_interface.0.nat_ip_address}"
-}
-
-
