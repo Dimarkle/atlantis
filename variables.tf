d9eea30d6d11c84b62e21982edd4c50829e14c7f
Dimarkle <118626944+Dimarkle@users.noreply.github.com>
2024-06-22 22:21:15 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,11 +5,13 @@ variable "token" {
 
 variable "cloud_id" {
   type        = string
+  default     = "b1g73ga4p1hchrvctbgo"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1gcms0oj5ro6jjsgqdg"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
